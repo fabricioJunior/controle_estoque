7ba67386332a10d4660d543ce639f64509f8a623
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2024-09-10 19:39:22 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ async function bootstrap() {
 
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:63263',
+      'https://estoque-online-e8o1ptnfp-fabriciojuniors-projects.vercel.app'
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
