9e142bdc321ad6fa74de478194206faa46aa4c3b
fabricioJunior <34898688+fabricioJunior@users.noreply.github.com>
2025-08-09 08:03:23 -0300

    add: cor

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,12 @@ async function bootstrap() {
       'https://estoque-online-git-main-fabriciojuniors-projects.vercel.app',
       'https://use-por-onde-flor-bcycwolyu-fabriciojuniors-projects.vercel.app',
       'https://use-por-onde-flor-git-master-fabriciojuniors-projects.vercel.app',
-      'https://www.useporondeflor.com.br'
+      'https://www.useporondeflor.com.br',
+      'useporondeflor.com.br',
+      'http://useporondeflor.com.br',
+      'https://useporondeflor.com.br'
     ],
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
   });
   await app.listen(process.env.PORT || 5080);
